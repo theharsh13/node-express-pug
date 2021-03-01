535e53f0a9808e6d7e3f5b840e5432613dd6d069
theharsh13 <harshvpatel24@gmail.com>
2021-03-01 19:16:39 +0530

    Refactoring code (product.js)
    -refactor code of file storage and overall product.js code

--- models/product.js
+++ models/product.js
@@ -2,18 +2,24 @@ const fs = require('fs')
 const path = require('path')
 const rootDir = require('../util/path')
 
+const p = path.join(rootDir, 'data', 'product.json')
+
+const getProductsFromFile = (callback)=>{
+    /* in order to get data callback function is required,
+        whenever file reading is done this callback will get called. */
+    fs.readFile(p, (err,fileContent)=>{
+        err ? callback([]) : callback(JSON.parse(fileContent))
+    })
+}
+
 module.exports = class Product {
     constructor(t){
         this.title = t
     }
 
     save() {
-        const p = path.join(rootDir, 'data', 'product.json')
-        fs.readFile(p, (err,fileContent)=>{
-            let products = []
-            if (!err) {
-                products = JSON.parse(fileContent)
-            }
+
+        getProductsFromFile((products)=>{
             products.push(this)
             fs.writeFile(p, JSON.stringify(products),  (err)=>{
                 console.log(err);
@@ -22,14 +28,6 @@ module.exports = class Product {
     }
 
     static fetchAll(callback){
-        /* in order to get data callback function is required,
-         whenever file reading is done this callback will get called. */
-        const p = path.join(rootDir, 'data', 'product.json')
-        fs.readFile(p, (err,fileContent)=>{
-            if(err){
-                callback([])
-            }
-            callback(JSON.parse(fileContent))
-        })
+        getProductsFromFile(callback)
     }
 }
