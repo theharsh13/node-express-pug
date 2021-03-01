7ebfc66f2929bfa9346d8830c23c5b138d6fcc08
theharsh13 <harshvpatel24@gmail.com>
2021-03-01 16:59:23 +0530

    MVC : adding model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -8,7 +8,8 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title)
+  product.save()
   res.redirect("/");
 };
 
@@ -17,6 +18,6 @@ exports.getProducts = (req,res,next)=>{
     res.render('shop', {
         docTitle: 'Shop view',
         path : '/',
-        prods : products
+        prods : Product.fetchAll()
     })
 }
